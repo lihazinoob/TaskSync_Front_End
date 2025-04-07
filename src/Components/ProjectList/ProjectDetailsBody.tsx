392c30ca770b7daa6f7d2f7a6904121908fae341
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2025-04-07 10:49:57 +0600

    The project detail body is created

---
+++ src/Components/ProjectList/ProjectDetailsBody.tsx
@@ -0,0 +1,10 @@
+export default function ProjectDetailsBody()
+{
+  return(
+    <>
+    <div>
+      
+    </div>
+    </>
+  );
+}
--- src/Components/ProjectList/ProjectDetailsLayout.tsx
+++ src/Components/ProjectList/ProjectDetailsLayout.tsx
@@ -1,11 +1,14 @@
 import ProjectDetailsHeader from "./ProjectDetailsHeader";
-
+import ProjectDetailsBody from "./ProjectDetailsBody";
 export default function ProjectDetailsLayout()
 {
   return(
     <>
       <div className="h-full w-full px-6">
         <ProjectDetailsHeader/>
+        <div className="mt-10">
+          <ProjectDetailsBody/>
+        </div>
       </div>
     </>
   );
